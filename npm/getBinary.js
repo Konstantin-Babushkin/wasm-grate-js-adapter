e37e40dc0607ff608f0e810e23c9d7444b7b3c3f
Konstantin-Babushkin <constant-babushkin@yandex.ru>
2023-10-30 23:03:33 +0300

    rewrite scripts

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -1,26 +1,82 @@
-const { Binary } = require('binary-install');
-const os = require('os');
+const { Binary } = require("binary-install");
+const os = require("os");
+const { join } = require("path");
+const cTable = require("console.table");
 
-const x64 = 'x64';
-function getPlatform() {
-    const type = os.type();
-    const arch = os.arch();
+const error = msg => {
+  console.error(msg);
+  process.exit(1);
+};
 
-    if (type === 'Windows_NT' && arch === x64) return 'win64';
-    if (type === 'Windows_NT') return 'win32';
-    if (type === 'Linux' && arch === x64) return 'linux';
-    if (type === 'Darwin' && arch === x64) return 'macos';
+const { version, name } = require("../package.json");
 
-    throw new Error(`Unsupported platform: ${type} ${arch}`);
-}
+const supportedPlatforms = [
+  {
+    TYPE: "Windows_NT",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-pc-windows-msvc",
+    BINARY_NAME: "wasm-grate-win.exe"
+  },
+  {
+    TYPE: "Linux",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-unknown-linux-musl",
+    BINARY_NAME: "wasm-grate-linux"
+  },
+  {
+    TYPE: "Darwin",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-apple-darwin",
+    BINARY_NAME: "wasm-grate-mac-x64"
+  },
+  {
+    TYPE: "Darwin",
+    ARCHITECTURE: "arm64",
+    RUST_TARGET: "x86_64-apple-darwin",
+    BINARY_NAME: "wasm-grate-mac-arm64"
+  }
+];
+
+const getPlatformMetadata = () => {
+  const type = os.type();
+  const architecture = os.arch();
+
+  for (let supportedPlatform of supportedPlatforms) {
+    if (
+      type === supportedPlatform.TYPE &&
+      architecture === supportedPlatform.ARCHITECTURE
+    ) {
+      return supportedPlatform;
+    }
+  }
+
+  error(
+    `Platform with type "${type}" and architecture "${architecture}" is not supported by ${name}.\nYour system must be one of the following:\n\n${cTable.getTable(
+      supportedPlatforms
+    )}`
+  );
+};
 
 const DOWNLOAD_PATH = 'https://github.com/Konstantin-Babushkin/wasm-grate/releases/download'
-function getBinary() {
-    const platform = getPlatform();
-    const version = require('../package.json').version;
-    const url = `${DOWNLOAD_PATH}/v${version}/wasm-grate-${platform}.tar.gz`;
-    const name = 'wasm-grate';
-    return new Binary(name, url);
-}
+const getBinary = () => {
+  const platformMetadata = getPlatformMetadata();
+  const url = `${DOWNLOAD_PATH}/v${version}/wasm-grate-${platformMetadata.RUST_TARGET}.tar.gz`;
+  return new Binary(platformMetadata.BINARY_NAME, url, version, {
+    installDirectory: join(__dirname, "node_modules", ".bin")
+  });
+};
 
-module.exports = getBinary;
+const run = () => {
+  const binary = getBinary();
+  binary.run();
+};
+
+const install = () => {
+  const binary = getBinary();
+  binary.install();
+};
+
+module.exports = {
+  install,
+  run
+};
